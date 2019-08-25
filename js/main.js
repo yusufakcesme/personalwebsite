0f787de1a41db1a4edca7eda6258087bcba9ca2d
yusufakcesme <yusufakcesme456@gmail.com>
2019-08-26 00:52:13 +0300

    added new elements and codes

--- js/main.js
+++ js/main.js
@@ -15,27 +15,44 @@ function dropdown() {
 
 //--------------------------------------------
 
-var btn = document.queryselectorAll('.btn');
-var image = document.queryselectorAll('.port-img');
 
+var btn = document.querySelectorAll('.btn');
+var images = document.querySelectorAll('.port-img');
 
 function selectItem() {
 
 	removeActive();
+	this.classList.add('active');
 
+	var name = this.innerText;
+
+	if (name == "BRAND") {
+		document.getElementById('pic-2').style.display = "none";
+	} else {
+		document.getElementById('pic-2').style.display = "block";
+	}
+
+	if (name == "DESIGN") {
+		document.getElementById('pic-4').style.display = "none";
+		document.getElementById('pic-6').style.display = "none";
+	} else {
+		document.getElementById('pic-4').style.display = "block";
+		document.getElementById('pic-6').style.display = "block";
+	}
+
+	if (name == "GRAPHIC") {
+		document.getElementById('pic-1').style.display = "none";
+		document.getElementById('pic-3').style.display = "none";
+	} else {
+		document.getElementById('pic-1').style.display = "block";
+		document.getElementById('pic-3').style.display = "block";
+	}
 
 }
-
 function removeActive() {
-	btn.forEach(item => item.classList.remove('active'));
+
+	btn.forEach(item => item.classList.remove("active"));
+
 }
-
-
-
-btn.forEach(item => item.addEventListener('click' selectItem));	
-
-
-
-
-
+btn.forEach(item => item.addEventListener('click', selectItem));
 
